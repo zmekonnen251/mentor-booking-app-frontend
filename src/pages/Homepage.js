c23e2add276aaede786a8fcc044dac372aec8cea
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-08-22 18:23:20 +0300

    Add cover backgroundd image with breakpoints

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import './homepage.scss';
 
 export default function Homepage() {
   return (
-    <h2>Homepage</h2>
+    <div class="container">
+      <div class="homepage">
+        <div class="homepage__overlay">
+          <h2>homepage</h2>
+        </div>
+      </div>
+    </div>
   );
 }
