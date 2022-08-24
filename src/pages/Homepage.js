e3127eec34d039be89afef27a7b4cb6e9be92bed
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-08-24 20:19:39 +0300

    Add hamburger menu button

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FaBars } from 'react-icons/fa';
 import Navbar from '../components/Navbar';
 import style from './Homepage.module.css';
 
 export default function Homepage() {
+  const [menu, setMenu] = useState(false);
+
+  const clickHandler = () => {
+    setMenu(!menu);
+  };
+
   return (
     <div className={style.homepage_container}>
       <div className={style.navbar}>
         <Navbar />
       </div>
       <div className={style.content}>
+        <button type="button" className={style.menubtn} onClick={() => { clickHandler(); }} aria-label="Hamburger menu">
+          <FaBars />
+        </button>
         <h2>Hello</h2>
       </div>
     </div>
