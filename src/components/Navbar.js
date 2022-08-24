17c43da8f118b5eafdaff12c35286733a6b9ea9f
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-08-24 17:15:05 +0300

    Add sidebar section and style it with pure css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import style from './Navbar.module.css';
 
 export default function Navbar() {
   return (
     <nav className={style.nav}>
-      <Link to="/">BookMentor</Link>
+      <Link to="/" className={style.logo}>BookAMentor</Link>
+      <div className={style.menu}>
+        <NavLink to="/" activeClassName={style.activelink}>Home</NavLink>
+        <NavLink to="/home">Reserve</NavLink>
+        <NavLink to="/shop">Shop</NavLink>
+        <NavLink to="/details">Details</NavLink>
+      </div>
+      <div className={style.footer}>
+        <li>facebook</li>
+      </div>
     </nav>
   );
 }
