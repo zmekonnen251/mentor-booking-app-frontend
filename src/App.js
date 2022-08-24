ef92435345d9fcf43fb0a877b2d9b083a85f3364
Adekunle Ismail <adismail4@gmail.com>
2022-08-24 21:57:28 +0100

    Turn off eslint quotes rule and comma-dangle

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import { Routes, Route } from 'react-router-dom';
-import routes from './routes';
+import { Routes, Route } from "react-router-dom";
+import routes from "./routes";
 
 function App() {
   return (
     <Routes>
       {routes.map((route) => (
-        <Route key={`key${route.path}`} path={route.path} element={route.element} exact={route.exact} />
+        <Route
+          key={`key${route.path}`}
+          path={route.path}
+          element={route.element}
+          exact={route.exact}
+        />
       ))}
     </Routes>
   );
