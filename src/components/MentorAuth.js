7dc2906cbf8e742a620714d6932d87c7fd4d4f6e
Zelalem <mekonnen.zelalem251@gmail.com>
2022-08-26 07:04:39 +0300

    integrate authentication with the rest of app

--- src/components/MentorAuth.js
+++ src/components/MentorAuth.js
@@ -1,184 +1,191 @@
+/* eslint-disable no-tabs */
+/* eslint-disable no-mixed-spaces-and-tabs */
 import React, { useState } from 'react';
 import { useNavigate, NavLink } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { signUpMentor, signInMentor } from '../redux/actions/auth';
 
-import '../styles/auth.scss';
+import './auth.css';
 
 const initialState = {
-	name: '',
-	email: '',
-	password: '',
-	confirmPassword: '',
-	image: '',
-	technologies: '',
-	bio: '',
+  name: '',
+  email: '',
+  password: '',
+  confirmPassword: '',
+  image: '',
+  technologies: '',
+  bio: '',
 };
 
 const MentorAuth = () => {
-	const dispatch = useDispatch();
-	const navigate = useNavigate();
-	const [mentorImg, setMentorImg] = useState(null);
-	const [isSignUp, setIsSignUp] = useState(false);
-	const [formData, setFormData] = useState(initialState);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [mentorImg, setMentorImg] = useState(null);
+  const [isSignUp, setIsSignUp] = useState(false);
+  const [formData, setFormData] = useState(initialState);
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
 
-	const handleSubmit = (event) => {
-		event.preventDefault();
-		validateForm();
-		setMentorImg(event.target.avatar.files[0]);
-	};
+  const validateEmail = (email, confirmEmail) => {
+    const re =			/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!isSignUp) {
+      return re.test(String(email).toLowerCase());
+    }
+    return re.test(String(email).toLowerCase()) && email === confirmEmail;
+  };
 
-	const validateEmail = (email, confirmEmail) => {
-		const re =
-			/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-		if (!isSignUp) {
-			return re.test(String(email).toLowerCase());
-		} else {
-			return re.test(String(email).toLowerCase()) && email === confirmEmail;
-		}
-	};
+  const validatePassword = (password, confirmPassword) => {
+    if (!isSignUp) {
+      return password.length > 6;
+    }
+    return password === confirmPassword && password.length > 6;
+  };
 
-	const validatePassword = (password, confirmPassword) => {
-		if (!isSignUp) {
-			return password.length > 6;
-		} else {
-			return password === confirmPassword && password.length > 6;
-		}
-	};
+  const validateForm = () => {
+    if (!validateEmail(formData.email, formData.confirmEmail)) {
+      setEmailError(true);
+    } else {
+      setEmailError(false);
+    }
 
-	const validateForm = () => {
-		console.log('validateForm');
-		if (!validateEmail(formData.email, formData.confirmEmail)) {
-			setEmailError(true);
-		} else {
-			setEmailError(false);
-		}
+    if (!validatePassword(formData.password, formData.confirmPassword)) {
+      setPasswordError(true);
+    } else {
+      setPasswordError(false);
+    }
 
-		if (!validatePassword(formData.password, formData.confirmPassword)) {
-			setPasswordError(true);
-		} else {
-			setPasswordError(false);
-		}
+    if (!emailError && !passwordError) {
+      if (isSignUp) {
+        const data = new FormData();
+        data.append('mentor[name]', formData.name);
+        data.append('mentor[email]', formData.email);
+        data.append('mentor[bio]', formData.email);
+        data.append('mentor[password]', formData.password);
+        data.append('mentor[avatar]', mentorImg);
 
-		if (!emailError && !passwordError) {
-			if (isSignUp) {
-				const data = new FormData();
-				data.append('mentor[name]', formData.name);
-				data.append('mentor[email]', formData.email);
-				data.append('mentor[bio]', formData.email);
-				data.append('mentor[password]', formData.password);
-				data.append('mentor[avatar]', mentorImg);
+        const technologies = formData.technologies.split(',');
+        const mentor = {
+          mentorData: data,
+          technologies,
+        };
 
-				const technologies = formData.technologies.split(',');
+        dispatch(signUpMentor(mentor, navigate, setIsSignUp));
+      } else {
+        const mentor = {
+          email: formData.email,
+          password: formData.password,
+        };
+        dispatch(signInMentor({ mentor }, navigate));
+      }
+    }
+  };
 
-				dispatch(signUpMentor(data, technologies, navigate, setIsSignUp));
-			} else {
-				const mentor = {
-					email: formData.email,
-					password: formData.password,
-				};
-				dispatch(signInMentor({ mentor }, navigate));
-			}
-		}
-	};
-	const handleChange = (event) => {
-		setFormData({ ...formData, [event.target.name]: event.target.value });
-	};
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    validateForm();
+    setMentorImg(event.target.avatar.files[0]);
+  };
 
-	const switchMode = () => {
-		setIsSignUp(!isSignUp);
-	};
+  const handleChange = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
 
-	return (
-		<div className='auth'>
-			<h1 className='auth__title'>{isSignUp ? 'Sign Up' : 'Sign In'}</h1>
-			<form className='auth__form' onSubmit={handleSubmit}>
-				{isSignUp && (
-					<input
-						type='text'
-						name='name'
-						placeholder='Name'
-						onChange={handleChange}
-						className='auth__form__input'
-					/>
-				)}
-				<input
-					type='email'
-					name='email'
-					placeholder='Email'
-					onChange={handleChange}
-					className='auth__form__input'
-				/>
-				{isSignUp && (
-					<input
-						type='email'
-						name='confirmEmail'
-						placeholder='Confirm Email'
-						onChange={handleChange}
-						className='auth__form__input'
-					/>
-				)}
-				<input
-					name='password'
-					type='password'
-					placeholder='Password'
-					onChange={handleChange}
-					className='auth__form__input'
-				/>
-				{isSignUp && (
-					<>
-						<input
-							type='password'
-							name='confirmPassword'
-							placeholder='Confirm Password'
-							onChange={handleChange}
-							className='auth__form__input'
-						/>
+  const switchMode = () => {
+    setIsSignUp(!isSignUp);
+  };
 
-						<input
-							type='text'
-							name='bio'
-							placeholder='Bio'
-							onChange={handleChange}
-							className='auth__form__input'
-						/>
+  return (
+    <div className="auth">
+      <h1 className="auth__title">{isSignUp ? 'Sign Up' : 'Sign In'}</h1>
+      <form className="auth__form" onSubmit={handleSubmit}>
+        {isSignUp && (
+        <input
+          type="text"
+          name="name"
+          placeholder="Name"
+          onChange={handleChange}
+          className="auth__form__input"
+        />
+        )}
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          onChange={handleChange}
+          className="auth__form__input"
+        />
+        {isSignUp && (
+        <input
+          type="email"
+          name="confirmEmail"
+          placeholder="Confirm Email"
+          onChange={handleChange}
+          className="auth__form__input"
+        />
+        )}
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          onChange={handleChange}
+          className="auth__form__input"
+        />
+        {isSignUp && (
+        <>
+          <input
+            type="password"
+            name="confirmPassword"
+            placeholder="Confirm Password"
+            onChange={handleChange}
+            className="auth__form__input"
+          />
 
-						<input
-							type='text'
-							name='technologies'
-							placeholder='Specializations'
-							onChange={handleChange}
-							className='auth__form__input'
-						/>
+          <input
+            type="text"
+            name="bio"
+            placeholder="Bio"
+            onChange={handleChange}
+            className="auth__form__input"
+          />
 
-						<span className='specialization'>
-							specializations separeted by comma!
-						</span>
+          <input
+            type="text"
+            name="technologies"
+            placeholder="Specializations"
+            onChange={handleChange}
+            className="auth__form__input"
+          />
 
-						<input
-							type='file'
-							name='avatar'
-							placeholder='Image'
-							onChange={handleChange}
-							className='auth__form__input'
-						/>
-					</>
-				)}
+          <span className="specialization">
+            specializations separeted by comma!
+          </span>
 
-				<button type='submit' className='auth__form__button'>
-					{isSignUp ? 'Submit Request' : 'Sign In'}
-				</button>
-				<button type='button' onClick={switchMode} className='auth__mode'>
-					{isSignUp
-						? 'Already have an account ? Sign in'
-						: "Don't have an account ? Sign Up"}
-				</button>
-				<NavLink to='/auth/user'>
-					<button className='auth__mode'>Sign in as a user</button>
-				</NavLink>
-			</form>
-		</div>
-	);
+          <input
+            type="file"
+            name="avatar"
+            placeholder="Image"
+            onChange={handleChange}
+            className="auth__form__input"
+          />
+        </>
+        )}
+
+        <button type="submit" className="auth__form__button">
+          {isSignUp ? 'Submit Request' : 'Sign In'}
+        </button>
+        <button type="button" onClick={switchMode} className="auth__mode">
+          {isSignUp
+					  ? 'Already have an account ? Sign in'
+					  : "Don't have an account ? Sign Up"}
+        </button>
+        <NavLink to="/auth/user">
+          <button className="auth__mode" type="button">
+            Sign in as a user
+          </button>
+        </NavLink>
+      </form>
+    </div>
+  );
 };
 
 export default MentorAuth;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,65 +1,91 @@
-import React from 'react';
-import { NavLink, Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import jwtDecode from 'jwt-decode';
 import {
-	FaFacebookF,
-	FaGooglePlusG,
-	FaInstagram,
-	FaLinkedinIn,
-	FaRegCopyright,
-	FaTwitter,
+  NavLink, Link, useLocation, useNavigate,
+} from 'react-router-dom';
+import {
+  FaFacebookF,
+  FaGooglePlusG,
+  FaInstagram,
+  FaLinkedinIn,
+  FaRegCopyright,
+  FaTwitter,
 } from 'react-icons/fa';
 import style from './Navbar.module.css';
+import { signOutUser } from '../redux/actions/auth';
 
 export default function Navbar() {
-	const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
+  const location = useLocation();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
 
-	// console.log(user.jwt);
-	// console.log(decode(user.jwt));
+  const logOut = () => {
+    setUser(null);
+    navigate('/auth/user');
+    dispatch(signOutUser());
+  };
 
-	const logOut = () => {
-		const profile = localStorage.getItem('profile');
-		console.log(profile);
-	};
-	const signUp = () => {};
-	const signIn = () => {};
+  useEffect(() => {
+    const token = user?.jwt;
+    if (token) {
+      const decoded = jwtDecode(token);
+      if (decoded.exp < Date.now() / 1000) {
+        logOut();
+      }
+    }
 
-	return (
-		<nav className={style.nav}>
-			<Link to='/' className={style.logo}>
-				BookAMentor
-			</Link>
-			<div className={style.menu}>
-				<NavLink to='/' activeclassname={style.activelink}>
-					Home
-				</NavLink>
-				<NavLink to='/home'>Reserve</NavLink>
-				<NavLink to='/shop'>Shop</NavLink>
-				<NavLink to='/details'>Details</NavLink>
-			</div>
+    setUser(JSON.parse(localStorage.getItem('profile')));
+  }, [location]);
 
-			<div className={style.footer}>
-				<ul className={style.social}>
-					<a href='facebook' target='_blank' aria-label='facebook'>
-						<FaFacebookF />
-					</a>
-					<a href='twitter' target='_blank' aria-label='twitter'>
-						<FaTwitter />
-					</a>
-					<a href='googleplus' target='_blank' aria-label='google'>
-						<FaGooglePlusG />
-					</a>
-					<a href='instagram' target='_blank' aria-label='instagram'>
-						<FaInstagram />
-					</a>
-					<a href='linkedin' target='_blank' aria-label='linkedin'>
-						<FaLinkedinIn />
-					</a>
-				</ul>
-				<p>
-					<FaRegCopyright />
-					2015 Microverse
-				</p>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className={style.nav}>
+      <Link to="/" className={style.logo}>
+        BookAMentor
+      </Link>
+      <div className={style.menu}>
+        {user ? (
+          <button className={style.logout} onClick={logOut} type="button">
+            Log out
+          </button>
+        ) : (
+          <>
+            <NavLink to="/auth/user">Log in</NavLink>
+          </>
+        )}
+
+        <NavLink to="/" active className={style.activelink}>
+          Home
+        </NavLink>
+        <NavLink to="/home">Reserve</NavLink>
+        <NavLink to="/shop">Shop</NavLink>
+        <NavLink to="/details">Details</NavLink>
+      </div>
+
+      <div className={style.footer}>
+        <ul className={style.social}>
+          <a href="facebook" target="_blank" aria-label="facebook">
+            <FaFacebookF />
+          </a>
+          <a href="twitter" target="_blank" aria-label="twitter">
+            <FaTwitter />
+          </a>
+          <a href="googleplus" target="_blank" aria-label="google">
+            <FaGooglePlusG />
+          </a>
+          <a href="instagram" target="_blank" aria-label="instagram">
+            <FaInstagram />
+          </a>
+          <a href="linkedin" target="_blank" aria-label="linkedin">
+            <FaLinkedinIn />
+          </a>
+        </ul>
+        <p>
+          <FaRegCopyright />
+          2015 Microverse
+        </p>
+      </div>
+    </nav>
+  );
 }
