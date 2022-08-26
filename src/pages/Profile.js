a44985dbfe6b30311e3d37b968c69ca398d4f759
zieeco <zieecosam@gmail.com>
2022-08-26 15:08:13 +0100

    Add profile html structure

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import style from './Profile.module.css';
 
 export default function Profile() {
   return (
-    <div>Profile</div>
+    <div className={style.user_container}>
+      <div className={style.user_details}>
+        <div className={style.img_cont}>
+          <img src="" alt="" />
+        </div>
+        <div className={style.card_bdy}>
+          <div className={`${style.text} ${style.d_flex}`}>
+            <h3>Wuletaw</h3>
+            <h3>Wonte</h3>
+          </div>
+          <p>example@mail.com</p>
+        </div>
+      </div>
+
+      <div className={style.user_contact}>
+        <form className={`${style.d_flex} ${style.class}`}>
+          <label htmlFor="user-name">
+            User name:
+            <input type="text" />
+          </label>
+          <label htmlFor="password">
+            Password:
+            <input type="text" />
+          </label>
+          <button type="submit">submit</button>
+        </form>
+      </div>
+    </div>
   );
 }
