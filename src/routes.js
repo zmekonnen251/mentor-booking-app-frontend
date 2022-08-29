769d3351805ea0101626a4104cac3bea31a15c13
Alfredbis29 <Kalumunabisimwa5@gmail.com>
2022-08-29 17:12:42 +0200

    add route for pending mentor and mentor list for admin

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,8 @@ import Homepage from './pages/Homepage';
 import UserAuth from './components/UserAuth';
 import MentorAuth from './components/MentorAuth';
 import Details from './pages/Details';
+import ApproveMentors from './components/ApproveMentors';
+import MentorsList from './components/MentorsList';
 
 const routes = [
   {
@@ -29,5 +31,19 @@ const routes = [
     element: <Details />,
   },
 ];
+
+{
+  path: 'admin/approve-mentors',
+  title: 'Approve Mentors',
+  exact: true,
+  element: <ApproveMentors />,
+  },
+
+  {
+  path: '/admin/approved-mentors',
+  title: 'Approved Mentors',
+  exact: true,
+  element: <MentorsList />,
+  },
 
 export default routes;
