2f51246350a50f2f99f38c802c322354f9a227b6
Zelalem <mekonnen.zelalem251@gmail.com>
2022-08-29 13:05:51 +0300

    create api end points for mentors

--- src/redux/api.js
+++ src/redux/api.js
@@ -1,22 +1,32 @@
+/* eslint-disable no-tabs */
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:3000/',
+	baseURL: 'http://localhost:3000/',
 });
 
 API.interceptors.request.use((req) => {
-  if (localStorage.getItem('profile')) {
-    req.headers.Authorization = `Bearer ${
-      JSON.parse(localStorage.getItem('profile')).jwt
-    }`;
-  }
-  return req;
+	if (localStorage.getItem('profile')) {
+		req.headers.Authorization = `Bearer ${
+			JSON.parse(localStorage.getItem('profile')).jwt
+		}`;
+	}
+	return req;
 });
 
-export const signUpUser = (data) => API.post('users', data);
-export const signInUser = (data) => API.post('users/sign_in', data);
-export const signOutUser = () => API.delete('users/sign_out');
-export const signUpMentor = (data) => API.post('mentors', data);
-export const signInMentor = (data) => API.post('mentors/sign_in', data);
-export const signOutMentor = () => API.delete('mentors/sign_out');
-export const addSpecialization = (data) => API.post('technologies', data);
+export const signUpUserApi = (data) => API.post('users', data);
+export const signInUserApi = (data) => API.post('users/sign_in', data);
+export const signOutUserApi = () => API.delete('users/sign_out');
+export const signUpMentorApi = (data) => API.post('mentors', data);
+export const signInMentorApi = (data) => API.post('mentors/sign_in', data);
+export const signOutMentorApi = () => API.delete('mentors/sign_out');
+export const addSpecializationApi = (data) => API.post('technologies', data);
+
+export const fetchMentorsApi = () => API.get('/mentors/list');
+export const approveMentorApi = (mentorId) =>
+	API.put('/approve_mentor', { id: mentorId, approved: true });
+
+export const banMentorApi = (mentorId) =>
+	API.put('/ban_mentor', { id: mentorId, approved: false });
+export const removeMentorApi = (mentorId) =>
+	API.delete(`/remove_mentor/${mentorId}`);
