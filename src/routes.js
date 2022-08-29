54585b55b0ecd1c3d19e9d81d4226599fc47cc5c
Alfredbis29 <Kalumunabisimwa5@gmail.com>
2022-08-29 17:29:16 +0200

    create peding mentors

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ import Homepage from './pages/Homepage';
 import UserAuth from './components/UserAuth';
 import MentorAuth from './components/MentorAuth';
 import Details from './pages/Details';
-import ApproveMentors from './components/ApproveMentors';
+import ApproveMentors from './components/PendingMentors';
 import MentorsList from './components/MentorsList';
 
 const routes = [
@@ -30,20 +30,20 @@ const routes = [
     exact: true,
     element: <Details />,
   },
-];
 
-{
-  path: 'admin/approve-mentors',
-  title: 'Approve Mentors',
-  exact: true,
-  element: <ApproveMentors />,
+  {
+    path: 'admin/approve-mentors',
+    title: 'Approve Mentors',
+    exact: true,
+    element: <ApproveMentors />,
   },
 
   {
-  path: '/admin/approved-mentors',
-  title: 'Approved Mentors',
-  exact: true,
-  element: <MentorsList />,
+    path: '/admin/approved-mentors',
+    title: 'Approved Mentors',
+    exact: true,
+    element: <MentorsList />,
   },
+];
 
 export default routes;
