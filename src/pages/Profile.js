a4b791cbc6d80a561ef2dd5366a69001f88d6210
zieeco <zieecosam@gmail.com>
2022-08-29 10:17:40 +0100

    Update the user image

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -2,11 +2,13 @@ import React from 'react';
 import style from './Profile.module.css';
 
 export default function Profile() {
+  const user = JSON.parse(localStorage.getItem('profile'));
+
   return (
     <div className={style.user_container}>
       <div className={style.user_details}>
         <div className={style.img_cont}>
-          <img src="https://robohash.org/etetvoluptate.png?size=300x300&set=set1" alt="User Avatar" />
+          <img src={`${user.user.avatar}`} alt="Avatar" />
         </div>
         <div className={style.card_bdy}>
           <div className={style.name}>
