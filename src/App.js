4f97e6ed8a2f5ca8cbb14cc29978996f37a0e6b0
zieeco <zieecosam@gmail.com>
2022-08-31 16:41:58 +0100

    Implement and dispatch the fetchReservation

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import Layout from './layouts/Layout';
 import { fetchMentors } from './redux/actions/mentors';
+import { fetchReservation } from './redux/actions/mentorReservation';
 
 function App() {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchMentors());
+    dispatch(fetchReservation());
   }, []);
   return <Layout />;
 }
