55b1170fa866ea26627e524706b9b30271ede809
zieeco <zieecosam@gmail.com>
2022-08-31 16:50:00 +0100

    Update the mentors action

--- src/redux/actions/mentors.js
+++ src/redux/actions/mentors.js
@@ -14,24 +14,21 @@ import {
 export const fetchMentors = () => async (dispatch) => {
   try {
     const res = await fetchMentorsApi();
-    console.log(res);
     const unapprovedMentors = res.data.unapproved_mentors;
     const approvedMentors = res.data.approved_mentors;
-    console.log({ unapprovedMentors, approvedMentors });
 
     dispatch({
       type: FETCH_MENTORS,
       payload: { unapprovedMentors, approvedMentors },
     });
   } catch (error) {
-    console.log(error);
+    // console.log(error);
   }
 };
 
 export const approveMentor = (mentorId) => async (dispatch) => {
   try {
     const res = await approveMentorApi(mentorId);
-    console.log(res.data);
     if (res.status === 200) {
       dispatch({
         type: APPROVE_MENTOR,
@@ -39,14 +36,13 @@ export const approveMentor = (mentorId) => async (dispatch) => {
       });
     }
   } catch (error) {
-    console.log(error);
+    // console.log(error);
   }
 };
 
 export const banMentor = (mentorId) => async (dispatch) => {
   try {
     const res = await banMentorApi(mentorId);
-    console.log(res.data);
     if (res.status === 200) {
       dispatch({
         type: BAN_MENTOR,
@@ -54,14 +50,13 @@ export const banMentor = (mentorId) => async (dispatch) => {
       });
     }
   } catch (error) {
-    console.log(error);
+    // console.log(error);
   }
 };
 
 export const removeMentor = (mentorId) => async (dispatch) => {
   try {
     const res = await removeMentorApi(mentorId);
-    console.log(res.data);
     if (res.status === 200) {
       dispatch({
         type: REMOVE_MENTOR,
@@ -69,6 +64,6 @@ export const removeMentor = (mentorId) => async (dispatch) => {
       });
     }
   } catch (error) {
-    console.log(error);
+    // console.log(error);
   }
 };
