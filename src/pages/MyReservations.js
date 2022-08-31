690a476aabd543cf332c8c35702f56a00f0e58c8
zieeco <zieecosam@gmail.com>
2022-08-31 16:44:16 +0100

    Update to user the original data

--- src/pages/MyReservations.js
+++ src/pages/MyReservations.js
@@ -1,19 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import { useSelector } from 'react-redux';
 import { TailSpin } from 'react-loading-icons';
 import style from './MyReservations.module.css';
 
 export default function MyReservations() {
-  const URL = 'https://random-data-api.com/api/v2/users?size=3';
-  const [mentors, setMentors] = useState([]);
-
-  useEffect(() => {
-    fetch(URL)
-      .then((response) => response.json())
-      .then((data) => {
-        setMentors(data);
-      })
-      .catch((error) => setMentors(error.message));
-  }, []);
+  const reservations = useSelector((state) => state.reservations);
 
   return (
     <div className={style.myreservations}>
@@ -21,7 +12,7 @@ export default function MyReservations() {
         <h1 className={style.maintitle}>
           My Reservations
         </h1>
-        <p>List of All reserved sessions with mentors</p>
+        <p>List of All reserved sessions with reservations</p>
       </div>
 
       <div className={style.mentorstable}>
@@ -34,17 +25,19 @@ export default function MyReservations() {
             </tr>
           </thead>
           <tbody>
-            {mentors.length === 0 ? <TailSpin />
-              : mentors.map((mentor) => (
-                <tr key={mentor.id}>
+            {reservations.length === 0 ? <TailSpin />
+              : reservations.map((reservation) => (
+                <tr key={reservation.id}>
                   <td className={style.mentordata}>
-                    <img src={mentor.avatar} alt={`${mentor.first_name} ${mentor.last_name}`} />
+                    <img src={reservation.avatar} alt={`${reservation.mentor_name}`} />
                     <div className={style.mentorname}>
-                      <h3>{`${mentor.first_name} ${mentor.last_name}`}</h3>
-                      <p>{mentor.email}</p>
+                      <h3>{reservation.mentor_name}</h3>
+                      <h3>{reservation.mentor_email}</h3>
+                      <p>{reservation.city}</p>
+                      <p>{reservation.country}</p>
                     </div>
                   </td>
-                  <td className="hr-center">{mentor.date_of_birth}</td>
+                  <td className="hr-center">{reservation.date}</td>
                   <td className="hr-center">
                     <button type="button" className={style.deletebtn}>Cancel</button>
                   </td>
