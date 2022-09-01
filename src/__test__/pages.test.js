3c275d5d2a57247bd361638d48623fdef105a88a
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-09-01 21:51:10 +0300

    Write unit tests for some of the pages of the app

---
+++ src/__test__/pages.test.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import configureStore from '../redux/store';
+import Homepage from '../pages/Homepage';
+import MyReservations from '../pages/MyReservations';
+import Layout from '../layouts/Layout';
+
+describe('Testing if all pages render correctly', () => {
+  it('Testing Homepage page', () => {
+    const tree = render(
+      <Provider store={configureStore}>
+        <Router>
+          <Homepage />
+        </Router>
+      </Provider>,
+    );
+    expect(tree).toMatchSnapshot();
+  });
+  it('Testing MyReservations page', () => {
+    const tree = render(
+      <Provider store={configureStore}>
+        <Router>
+          <MyReservations />
+        </Router>
+      </Provider>,
+    );
+    expect(tree).toMatchSnapshot();
+  });
+  it('Testing Layout Component', () => {
+    const tree = render(
+      <Provider store={configureStore}>
+        <Layout />
+      </Provider>,
+    );
+    expect(tree).toMatchSnapshot();
+  });
+});
