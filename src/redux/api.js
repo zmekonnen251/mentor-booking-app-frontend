c89c46191d419a97eb57f30a8be8acea582c4812
Zelalem <mekonnen.zelalem251@gmail.com>
2022-10-06 20:39:57 +0300

    Change baseUrl to heroku

--- src/redux/api.js
+++ src/redux/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://127.0.0.1:3000',
+  baseURL: 'https://mentorbooking.herokuapp.com/',
 });
 
 API.interceptors.request.use((req) => {
